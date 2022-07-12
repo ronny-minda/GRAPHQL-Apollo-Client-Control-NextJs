9ddf4a1a8fa36720e34337891836ca66990a8014
ronny-minda <ronny.michael.minda.vera@gmail.com>
2022-07-12 11:44:19 -0500

    terminado

--- components/Cliente.js
+++ components/Cliente.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Swal from "sweetalert2";
 import { gql, useMutation } from "@apollo/client";
+import Router from "next/router";
 
 const ELIMINAR_CLIENTE = gql`
   mutation Mutation($eliminarClienteId: ID!) {
@@ -72,6 +73,13 @@ const Cliente = ({ cliente }) => {
     });
   };
 
+  const editarClienteCliente = (id) => {
+    Router.push({
+      pathname: "/editarcliente/[id]",
+      query: { id },
+    });
+  };
+
   return (
     <tr>
       <td className="border px-4 py-2">
@@ -98,6 +106,30 @@ const Cliente = ({ cliente }) => {
               strokeLinejoin="round"
               strokeWidth="2"
               d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+            ></path>
+          </svg>
+        </button>
+      </td>
+
+      <td className="border px-4 py-2">
+        <button
+          type="button"
+          className="flex justify-center items-center bg-green-600 py-2 px-4 w-full text-white rounded text-xs uppercase font-bold"
+          onClick={() => editarClienteCliente(cliente.id)}
+        >
+          Editar
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
             ></path>
           </svg>
         </button>
--- components/Header.js
+++ components/Header.js
@@ -19,9 +19,9 @@ const Header = () => {
 
   const { data, loading, error } = useQuery(OBTENER_USUARIO);
 
-  console.log(data);
-  console.log(loading);
-  console.log(error);
+  // console.log(data);
+  // console.log(loading);
+  // console.log(error);
 
   if (loading) return <p>CARGADO........</p>;
 
